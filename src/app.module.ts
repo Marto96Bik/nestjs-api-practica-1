3a45c0d54f7614ecd0cbb50e85964eefd52febfc
Marto96Bik <martobik@gmail.com>
2025-08-04 22:43:10 +0300

    Adapted the file app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from './app.datasource';
 
 @Module({
-  imports: [
-    UserModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '', // Deja vacío si no configuraste contraseña
-      database: 'nestjs-api-practica-1-db', // Crea esta base después
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Usa false en producción
-    }),
-  ],
+  imports: [UserModule, TypeOrmModule.forRoot(AppDataSource.options)],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: 'AppDataSource',
+      useFactory: async () => AppDataSource,
+    },
+  ],
 })
 export class AppModule {}
